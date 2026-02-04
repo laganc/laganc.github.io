a731d880e92a7abadb8704d323331b5848d0d4d4
laganchohan <lchohan@uvic.ca>
2026-02-04 00:22:52 -0800

    add another gif

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const noSound = document.getElementById('noSound');
 
 yesBtn.addEventListener('click', () => {
     responseText.textContent = 'AWW, I LOVE YOU MOREEEEEE! 💖 💞';
-    gif.src = 'https://i.pinimg.com/originals/b4/65/34/b46534530b0ef3ffac6636f068dd2e12.gif';
+    gif.src = 'yes.gif';
     yesBtn.style.display = 'none';
     noBtn.style.display = 'none';
     title.style.color = 'transparent';
