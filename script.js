96395c30d682b19635e398d324be811a9caf8390
laganchohan <lchohan@uvic.ca>
2026-02-03 23:40:02 -0800

    update lang

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ noBtn.addEventListener('mouseover', () => {
 });
 
 noBtn.addEventListener('click', () => {
-    responseText.textContent = 'Нет? Ну ладно, но ты всё равно моя валентинка! 🤭';
+    responseText.textContent = 'It\'s okay, I still love you 🤭';
     gif.src = 'https://i.pinimg.com/originals/3e/47/7e/3e477e83c35e2a7a38f19ccdad163faa.gif';
     yesBtn.style.display = 'none';
     noBtn.style.display = 'none';
